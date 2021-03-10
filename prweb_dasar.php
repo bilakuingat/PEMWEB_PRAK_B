2cf9f6fbf764226474dc5222a7f2f1416cad2ad1
Aura Salsabila <72138007+bilakuingat@users.noreply.github.com>
2021-03-10 20:18:17 +0700

    10/3/2020
    praktikum 1

---
+++ prweb_dasar.php
@@ -0,0 +1,18 @@
+<!Document html>
+<html lang="en">
+<head> 
+    <meta charset ="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content ="IE=edge">
+    <meta name="viewport" content = "width=device-width, initial-scale=1.0">
+    <title> DASAR PHP</title> 
+</head>
+<body>
+    <?php for($i = 0; $i < 5; $i++): ?>
+        <?php if($i % 2 === 0): ?> 
+            <h2> hello word </h2>
+        <?php else : ?>
+            <h1> Hello world </h2>
+        <?php endif; ?> 
+    <?php endfor;?> 
+</body>
+</html>
